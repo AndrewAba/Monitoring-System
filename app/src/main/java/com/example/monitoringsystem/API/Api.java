470f0458e9a1229e0279a993f5211199d365df0c
Abalasei Andrei <43205655+FluffsPuffs@users.noreply.github.com>
2020-05-22 14:39:37 +0300

    push

--- app/src/main/java/com/example/monitoringsystem/API/Api.java
+++ app/src/main/java/com/example/monitoringsystem/API/Api.java
@@ -1,11 +1,12 @@
 package com.example.monitoringsystem.API;
 import com.example.monitoringsystem.model.Parameters;
+
 import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface Api {
     @GET("/sensor")
     Call<List<Parameters>> getMeasurement();
-
 }
--- app/src/main/java/com/example/monitoringsystem/API/ServiceGenerator.java
+++ app/src/main/java/com/example/monitoringsystem/API/ServiceGenerator.java
@@ -1,6 +1,5 @@
 package com.example.monitoringsystem.API;
 
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
